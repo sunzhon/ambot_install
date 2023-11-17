98609c1154171c734c20f03f8ce5d0edf073efae
suntao <suntao@ambot.com>
2023-11-17 10:36:57 +0800

    update tool installtion

--- install_tools.sh
+++ install_tools.sh
@@ -2,6 +2,9 @@
 
 sudo apt-get install cmake g++ gcc && \
 
+RUN apt-get update && apt-get install -y sudo vim curl pkg-config build-essential ninja-build automake autoconf libtool wget curl git gcc libssl-dev bc slib squashfs-tools  tree python3-dev python3-pip device-tree-compiler ssh cpio fakeroot libncurses5 libncurses5-dev genext2fs rsync unzip mtools tclsh ssh-client  && \
+    apt-get install -y vim gcc g++ cmake libgsl-dev make git
+
 
     echo "installation Done!"
 
